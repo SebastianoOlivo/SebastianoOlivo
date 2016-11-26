2d57d6f96346b128db834d2b1df2177e7cb7000b
Sebastiano Olivo <olivo.sebastiano@gmail.com>
2016-11-26 21:36:52 +0100

    new menu and project section wip

--- assets/js/modules/navigation.js
+++ assets/js/modules/navigation.js
@@ -1,15 +1,36 @@
 var body = document.getElementsByTagName('body')[0],
     container = body.querySelectorAll('.container')[0],
     navBtn = container.querySelectorAll('.nav-btn')[0],
-    navItems = container.querySelectorAll('.mainnav')[0];
+    navItems = container.querySelectorAll('.mainnav__item'),
+    activeItem = "";
+
 
 function toggleMenu() {
     container.classList.toggle('menu-active');
 }
 
+function _menuActions(event) {
+    event.preventDefault();
+    var elem = event.currentTarget,
+        elemAtt = elem.getAttribute("data-navBtn");
+    if (activeItem !== "") {
+        container.classList.remove(activeItem);
+    }
+    if (elemAtt !== "") {
+        event.preventDefault();
+        container.classList.add(elemAtt);
 
+    } else {
+
+
+    }
+    activeItem = elemAtt;
+    toggleMenu();
+}
 
 window.addEventListener("load", function() {
     navBtn.addEventListener('click', toggleMenu);
-    navItems.addEventListener('click', toggleMenu);
+    for (var i = navItems.length; i--;) {
+        navItems[i].addEventListener('click', _menuActions);
+    }
 });
