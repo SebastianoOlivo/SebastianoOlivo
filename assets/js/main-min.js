7218eae81391f82d398a3a11000bf7b96ff91071
Sebastiano Olivo <olivo.sebastiano@gmail.com>
2016-11-30 11:21:09 +0100

    add vimeo api script

--- assets/js/main-min.js
+++ assets/js/main-min.js
@@ -46,17 +46,5 @@ window.addEventListener("load", function() {
 });
 
 
-var project = document.querySelector('.project'),
-showMore = project.querySelector('.show-more');
-
-function toggleMoreInfo() {
-  project.classList.toggle('project__info--unfold');
-}
-
-window.addEventListener('load', function() {
-  showMore.addEventListener('click', toggleMoreInfo, false);
-});
-
-
 
 
