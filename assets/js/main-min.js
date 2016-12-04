1a2c3b5edd954a0ea14c6b58238d4b41bcb46944
Sebastiano Olivo <olivo.sebastiano@gmail.com>
2016-12-04 21:02:07 +0100

    improve navigation and start to clean code

--- assets/js/main-min.js
+++ assets/js/main-min.js
@@ -12,6 +12,8 @@ var body = document.getElementsByTagName('body')[0],
     container = body.querySelectorAll('.container')[0],
     navBtn = container.querySelectorAll('.nav-btn')[0],
     navItems = container.querySelectorAll('.mainnav__item'),
+    footerNavItems = container.querySelectorAll('.footer-nav__item'),
+    projectBtn = document.querySelector('.project-btn'),
     activeItem = "";
 
 
@@ -20,6 +22,8 @@ function toggleMenu() {
 }
 
 function _menuActions(event) {
+  var url = event.getElementsByTagName('A')[0];
+  url = url.getAttribute('href');
     event.preventDefault();
     var elem = event.currentTarget,
         elemAtt = elem.getAttribute("data-navBtn");
@@ -31,7 +35,7 @@ function _menuActions(event) {
         container.classList.add(elemAtt);
 
     } else {
-
+      window.location.href = url;
 
     }
     activeItem = elemAtt;
@@ -40,8 +44,14 @@ function _menuActions(event) {
 
 window.addEventListener("load", function() {
     navBtn.addEventListener('click', toggleMenu);
+    if(projectBtn != null) {
+      projectBtn.addEventListener('click', _menuActions);
+    }
     for (var i = navItems.length; i--;) {
         navItems[i].addEventListener('click', _menuActions);
+    }
+    for (var i = footerNavItems.length; i--;) {
+        footerNavItems[i].addEventListener('click', _menuActions);
     }
 });
 
--- assets/js/modules/navigation.js
+++ assets/js/modules/navigation.js
@@ -2,6 +2,8 @@ var body = document.getElementsByTagName('body')[0],
     container = body.querySelectorAll('.container')[0],
     navBtn = container.querySelectorAll('.nav-btn')[0],
     navItems = container.querySelectorAll('.mainnav__item'),
+    footerNavItems = container.querySelectorAll('.footer-nav__item'),
+    projectBtn = document.querySelector('.project-btn'),
     activeItem = "";
 
 
@@ -10,6 +12,8 @@ function toggleMenu() {
 }
 
 function _menuActions(event) {
+  var url = event.getElementsByTagName('A')[0];
+  url = url.getAttribute('href');
     event.preventDefault();
     var elem = event.currentTarget,
         elemAtt = elem.getAttribute("data-navBtn");
@@ -21,7 +25,7 @@ function _menuActions(event) {
         container.classList.add(elemAtt);
 
     } else {
-
+      window.location.href = url;
 
     }
     activeItem = elemAtt;
@@ -30,7 +34,13 @@ function _menuActions(event) {
 
 window.addEventListener("load", function() {
     navBtn.addEventListener('click', toggleMenu);
+    if(projectBtn != null) {
+      projectBtn.addEventListener('click', _menuActions);
+    }
     for (var i = navItems.length; i--;) {
         navItems[i].addEventListener('click', _menuActions);
+    }
+    for (var i = footerNavItems.length; i--;) {
+        footerNavItems[i].addEventListener('click', _menuActions);
     }
 });
